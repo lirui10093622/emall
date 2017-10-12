f80bfbb3914f3546b5940ec95de9fa6af8d87c24
rui.li04 <rui.li04@mljr.com>
2017-10-12 14:29:29 +0800

    第一次提交

---
+++ emall-commodity/src/main/java/org/lr/expriment/emall/commodity/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author 李锐
+ * @email rui.li04@mljr.com
+ * @time 2017-10-12 14:08:23
+ */
+package org.lr.expriment.emall.commodity;
---
+++ emall-pay/src/main/java/org/lr/expriment/emall/pay/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author 李锐
+ * @email rui.li04@mljr.com
+ * @time 2017-10-12 14:26:28
+ */
+package org.lr.expriment.emall.pay;
