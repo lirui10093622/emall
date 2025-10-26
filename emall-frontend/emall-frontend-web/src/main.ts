f9812db2fb2b7b2b5afafcbf5f5cd1917d1a0e81
lirui <lirui@nl-ds.cn>
2025-10-26 11:00:57 +0800

    element-plus的组件

--- emall-frontend/emall-frontend-web/src/main.ts
+++ emall-frontend/emall-frontend-web/src/main.ts
@@ -7,8 +7,11 @@ import 'element-plus/dist/index.css'
 import Buttons from './buttons/Buttons.vue'
 import App from './App.vue'
 import ContainerLayout from './layout/ContainerLayout.vue'
+import Login from './login/Login.vue'
+import DefaultMenu from './menu/DefaultMenu.vue'
+import CustomMenu from './menu/CustomMenu.vue'
 
 // createApp(App).mount('#app')
 
 
-createApp(ContainerLayout).use(ElementPlus).mount('#app')
+createApp(CustomMenu).use(ElementPlus).mount('#app')
